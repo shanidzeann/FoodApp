fe9c04b278e5d4e9723ad14704d2e100b1993cd3
shanidzeann <shanidzeann@mail.ru>
2022-03-25 12:29:18 +0300

    Delete storyboard

---
+++ FoodApp/AppDelegate/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 25.03.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        window?.windowScene = windowScene
+    }
+    
+}
+
