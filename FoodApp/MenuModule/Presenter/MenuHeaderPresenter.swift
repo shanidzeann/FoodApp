d1413da48e03782cc8200d856f2b4aa785774ee9
shanidzeann <shanidzeann@mail.ru>
2022-03-25 15:33:30 +0300

    Create files for header

---
+++ FoodApp/MenuModule/Presenter/MenuHeaderPresenter.swift
@@ -0,0 +1,8 @@
+//
+//  MenuHeaderPresenter.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 25.03.2022.
+//
+
+import Foundation
---
+++ FoodApp/MenuModule/Protocols/MenuHeaderPresenterProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  MenuHeaderPresenterProtocol.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 25.03.2022.
+//
+
+import Foundation
