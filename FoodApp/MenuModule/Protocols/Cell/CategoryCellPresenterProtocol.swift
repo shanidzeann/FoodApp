67d2a7cfae7a37d6d82b28e61883191c48ee705a
shanidzeann <shanidzeann@mail.ru>
2022-03-28 01:21:30 +0300

    Create protocols

---
+++ FoodApp/MenuModule/Protocols/Cell/CategoryCellPresenterProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCellPresenterProtocol.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 28.03.2022.
+//
+
+import Foundation
+
+protocol CategoryCellPresenterProtocol {
+    
+}
---
+++ FoodApp/MenuModule/Protocols/Cell/CategoryCellProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  CategoryCellProtocol.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 28.03.2022.
+//
+
+import Foundation
+
+protocol CategoryCellProtocol: AnyObject {
+    
+}
