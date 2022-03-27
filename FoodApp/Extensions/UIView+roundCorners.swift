b4a8ebebd07a510e53239e1cc0a688eb1bf1a65c
shanidzeann <shanidzeann@mail.ru>
2022-03-28 00:33:00 +0300

    Create UIView extension

---
+++ FoodApp/Extensions/UIView+roundCorners.swift
@@ -0,0 +1,17 @@
+//
+//  UIView+roundCorners.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 28.03.2022.
+//
+
+import UIKit
+
+extension UIView {
+   func roundCorners(corners: UIRectCorner, radius: CGFloat) {
+        let path = UIBezierPath(roundedRect: bounds, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        let mask = CAShapeLayer()
+        mask.path = path.cgPath
+        layer.mask = mask
+    }
+}
