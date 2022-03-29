c5508f75d77b42a343444c29e9643c086a23681a
shanidzeann <shanidzeann@mail.ru>
2022-03-29 15:44:12 +0300

    Set categoryCcollectionView item size

---
+++ FoodApp/MenuModule/View/MenuView/Controller/MenuViewController+UICollectionViewDelegateFlowLayout.swift
@@ -0,0 +1,14 @@
+//
+//  MenuViewController+UICollectionViewDelegateFlowLayout.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 29.03.2022.
+//
+
+import UIKit
+
+extension MenuViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: presenter.sections[indexPath.item].size(withAttributes: [NSAttributedString.Key.font : UIFont.systemFont(ofSize: 17)]).width + 25, height: 35)
+    }
+}
