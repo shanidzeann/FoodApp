923d744f80b8d3645ac0ccec9cfe638b659116a8
shanidzeann <shanidzeann@mail.ru>
2022-03-29 15:46:37 +0300

    Enable home pan gesture recognizer when user scrolls to top of menu

--- FoodApp/MenuModule/View/MenuView/Controller/MenuViewController+UIScrollViewDelegate.swift
+++ FoodApp/MenuModule/View/MenuView/Controller/MenuViewController+UIScrollViewDelegate.swift
@@ -9,7 +9,10 @@ import UIKit
 
 extension MenuViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        if scrollView.bounds.minY == 0, let collectionView = scrollView as? UICollectionView, collectionView.tag == 1 {
+        if scrollView.bounds.minY == 0,
+           let collectionView = scrollView as? UICollectionView,
+           let collectionViewType = CollectionViewType(rawValue: collectionView.tag),
+           collectionViewType == .menu {
             NotificationCenter.default.post(name: NSNotification.Name("enableHomePanGestureRecognizer"), object: nil)
         }
     }
