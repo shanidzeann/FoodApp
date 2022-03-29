e5298989559f27e758b157202dbb0fcf96e42468
shanidzeann <shanidzeann@mail.ru>
2022-03-29 15:43:06 +0300

    Scroll to selected category

--- FoodApp/MenuModule/View/MenuView/Controller/MenuViewController+UICollectionViewDelegate.swift
+++ FoodApp/MenuModule/View/MenuView/Controller/MenuViewController+UICollectionViewDelegate.swift
@@ -9,4 +9,13 @@ import UIKit
 
 extension MenuViewController: UICollectionViewDelegate {
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let collectionViewType = CollectionViewType(rawValue: collectionView.tag)
+        
+        if collectionViewType == .categories {
+            let indexPath = IndexPath(item: 0, section: indexPath.item)
+            menuCollectionView?.scrollToItem(at: indexPath, at: .top, animated: true)
+        }
+    }
+    
 }
