a124a45ab0bf1664c4201635f0e2e7b5a75d1a69
shanidzeann <shanidzeann@mail.ru>
2022-03-30 23:21:36 +0300

    Change color of items count in cell using attributed string

---
+++ FoodApp/MenuModule/View/MenuView/Controller/BottomCard/CardViewController+UITableViewDataSource.swift
@@ -0,0 +1,28 @@
+//
+//  CardViewController+UITableViewDataSource.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 30.03.2022.
+//
+
+import UIKit
+
+extension CardViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sections.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let cellText = "\(sections[indexPath.row].title) \(sections[indexPath.row].itemsCount)"
+        let itemsCount = "\(sections[indexPath.row].itemsCount)"
+        
+        let range = (cellText as NSString).range(of: itemsCount)
+        
+        let attributedString = NSMutableAttributedString(string: cellText)
+        attributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.gray, range: range)
+        
+        cell.textLabel?.attributedText = attributedString
+        return cell
+    }
+}
