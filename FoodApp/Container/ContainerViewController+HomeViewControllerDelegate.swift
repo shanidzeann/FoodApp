779cc04cebe4a7d46f6580bd07420224ad20d3bf
shanidzeann <shanidzeann@mail.ru>
2022-04-23 11:57:54 +0300

    Refactor

--- FoodApp/Container/ContainerViewController+HomeViewControllerDelegate.swift
+++ FoodApp/Container/ContainerViewController+HomeViewControllerDelegate.swift
@@ -14,40 +14,42 @@ extension ContainerViewController: HomeViewControllerDelegate {
     
     func toggleMenu() {
         NotificationCenter.default.post(name: NSNotification.Name("animateTransitionBeforeSideMenu"), object: nil)
+        
         switch sideMenuState {
         case .closed:
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
-                self.menuVC.view.frame.origin.x = 0
-                self.navController?.view.frame.origin.x = self.frame.size.width * 0.75
-                self.navController?.view.frame.origin.y = self.frame.size.height * 0.05
-                self.navController?.view.frame.size.height = self.frame.size.height * 0.9
-                
-                self.shadowView?.frame.origin.x = self.safeNavBarFrame.size.width * 0.75
-                self.shadowView?.frame.origin.y = self.safeNavBarFrame.size.height * 0.05
-                self.shadowView?.frame.size.height = self.safeNavBarFrame.size.height * 0.9
-                
-                self.homeVC.bannerViewController.collectionView?.collectionViewLayout.invalidateLayout()
-                self.homeVC.menuViewController.moreButton.isUserInteractionEnabled = false
+                self.animateMenu(menuX: 0, vcX: 0.75, vcY: 0.05, vcHeight: 0.9)
             } completion: { [weak self] done in
                 if done {
-                    self?.sideMenuState = .opened
+                    self?.menuAnimationCompletion(isOpened: true)
                 }
             }
         case .opened:
             UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0, options: .curveEaseInOut) {
-                self.menuVC.view.frame.origin.x = -self.frame.width * 0.7
-                self.navController?.view.frame.origin.x = 0
-                self.navController?.view.frame.origin.y = 0
-                self.navController?.view.frame.size.height = self.frame.size.height
-                
-                self.shadowView?.frame.origin.x = 0
-                self.shadowView?.frame.origin.y = 0
-                self.shadowView?.frame.size.height = self.safeNavBarFrame.size.height
-                
-                self.homeVC.menuViewController.moreButton.isUserInteractionEnabled = true
+                self.animateMenu(menuX: -0.7, vcX: 0, vcY: 0, vcHeight: 1)
             } completion: { [weak self] done in
-                self?.sideMenuState = .closed
+                self?.menuAnimationCompletion(isOpened: false)
             }
         }
+    }
+    
+    private func animateMenu(menuX: Double, vcX: Double, vcY: Double, vcHeight: Double) {
+        self.menuVC.view.frame.origin.x = self.frame.width * menuX
+        self.navController?.view.frame = CGRect(x: self.frame.size.width * vcX,
+                           y: self.frame.size.height * vcY,
+                           width: self.frame.size.width,
+                           height: self.frame.size.height * vcHeight)
+        self.shadowView?.frame = CGRect(x: self.safeNavBarFrame.size.width * vcX,
+                                     y: self.safeNavBarFrame.size.height * vcY,
+                                     width: self.safeNavBarFrame.size.width,
+                                     height: self.safeNavBarFrame.size.height * vcHeight)
+    }
+    
+    private func menuAnimationCompletion(isOpened: Bool) {
+        if isOpened {
+            homeVC.bannerViewController.collectionView?.collectionViewLayout.invalidateLayout()
+        }
+        homeVC.menuViewController.moreButton.isUserInteractionEnabled = !isOpened
+        sideMenuState.toggle()
     }
 }
--- FoodApp/SideMenuModule/Model/SideMenuState.swift
+++ FoodApp/SideMenuModule/Model/SideMenuState.swift
@@ -10,4 +10,13 @@ import Foundation
 enum SideMenuState {
     case opened
     case closed
+    
+    mutating func toggle() {
+        switch self {
+        case .opened:
+            self = .closed
+        case .closed:
+            self = .opened
+        }
+    }
 }
