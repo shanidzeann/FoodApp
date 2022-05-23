b6c8b2f570c6d6a816a7a9ec14ac08ee0a3f3f44
shanidzeann <shanidzeann@mail.ru>
2022-05-23 10:50:58 +0300

    Create AuthManager

---
+++ FoodApp/Managers/AuthManager.swift
@@ -0,0 +1,12 @@
+//
+//  AuthManager.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 04.05.2022.
+//
+
+import Foundation
+
+class AuthManager: AuthManagerProtocol {
+    
+}
---
+++ FoodApp/Managers/AuthManagerProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  AuthManagerProtocol.swift
+//  FoodApp
+//
+//  Created by Anna Shanidze on 04.05.2022.
+//
+
+import Foundation
+
+protocol AuthManagerProtocol {
+    
+}
