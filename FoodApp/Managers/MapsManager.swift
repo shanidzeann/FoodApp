3e612a3bdac8b6e11f56ccb31e4b0b69702063cd
shanidzeann <shanidzeann@mail.ru>
2022-06-14 18:12:38 +0300

    Refactor

--- FoodApp/Managers/MapsManager.swift
+++ FoodApp/Managers/MapsManager.swift
@@ -10,9 +10,8 @@ import MapKit
 
 class MapsManager: MapsManagerProtocol {
     func loadInitialData() -> [Restaurant]? {
-        guard
-            let fileName = Bundle.main.url(forResource: "restaurants", withExtension: "geojson"),
-            let restaurantsData = try? Data(contentsOf: fileName) else { return nil }
+        guard let fileName = Bundle.main.url(forResource: "restaurants", withExtension: "geojson"),
+              let restaurantsData = try? Data(contentsOf: fileName) else { return nil }
         do {
             let features = try MKGeoJSONDecoder().decode(restaurantsData).compactMap { $0 as? MKGeoJSONFeature }
             let valid = features.compactMap(Restaurant.init)
